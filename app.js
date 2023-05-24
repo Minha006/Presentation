45f12f756571964de38b72a372548573fb940d7e
Nageen Yousuf <homayonyousaf@gmail.com>
2023-05-25 01:35:32 +0500

    created index.html

---
+++ app.js
@@ -0,0 +1,152 @@
+ // == It checks just value of the variable
+// let num1 = 7;
+// let num2 = 8;
+// console.log(num1 == num2);
+//   //!= Not equal
+// let num3 = 9;
+// let num4 = 10;
+// console.log(num3 != num4);
+
+// //=== It check value of the variable as well as type of the variable
+// // let nmb1 = 45;
+// // let nmb2 = "45";
+// // console.log(nmb1 === nmb2);
+//   //!==
+//   let nmb1 = 45;
+//   let nmb2 = "45";
+//   console.log(nmb1 !== nmb2);
+
+//comparison operator 
+//---------->------
+let x = 45;
+let y = 50;
+console.log(x > y);
+
+//-----<------
+let num1 = 6;
+let num2 = 7
+console.log(num1 < num2);
+
+//>=
+//1) 
+  var yourAge = 19;
+    if (yourAge >= 18) {
+    document.write("You are eligible to vote.");
+   } else {
+    document.write("You are not yet eligible to vote.");
+    };
+
+//2)
+    let marks = 85;
+    if (marks >= 90) {
+      console.log("You received an A grade.");
+    } else if (marks >= 80) {
+      console.log("You received a B grade.");
+    } else if (marks >= 70) {
+      console.log("You received a C grade.");
+    } else {
+      console.log("You need to improve your score.");
+    }
+    //Example
+    let points = 75;
+    let passingPoints = 60;
+     if (points >= passingPoints) {
+      
+  console.log("Congratulations! You passed the exam.");
+} else {
+  console.log("Unfortunately, you did not pass the exam.");
+}  
+
+     //<=
+     //1)
+     let age = 18;
+if (age <= 18) {
+  console.log("You are eligible for a younger discount.");
+} else {
+  console.log("Regular ticket prices apply.");
+}
+
+//2)
+   let childAge = 5;
+   if(age<= 5){
+    console.log("Your child can take polio drops")
+   }else{
+    console.log("No need for polio drops");
+   }
+
+   //Example 
+   let temp = 38;
+   if(temp <= 38){
+    document.write("Lets go to have fun")
+}else{
+   document.write("It's better to stay at home")
+
+}
+
+
+//var x = "45";
+//var y = "45";
+
+//console.log(x == y);
+//console.log(x !== y);
+//console.log(x >= y);
+//console.log(x < y);
+//console.log(null == undefined); its true 
+//console.log(null === undefined);its false bcz its data type is also change
+// if is a condition--true or false
+//agr condition true hoi to hi wo chlygi or agr wo false hoi to out hojay gi
+
+//if(x == y){
+
+//alert("Game Start");
+//}
+// var meal = "biryani";
+// var meal2 = "chicken tikka";
+// if(meal = "biryani"){
+//     alert("Arry Waah");
+    
+// }else{
+//     alert("ye bh theek h" );
+// };
+//  var weather = "rainy";
+// if (weather === "rainy") {
+//  console.log("Take an umbrella with you.");
+//  } else {
+//    console.log("No need for an umbrella today.");
+//  }
+
+//  var age = 17;
+//  if (age >= 18) {
+//    console.log("You are eligible to vote.");
+//  } else {
+//    console.log("You are not yet eligible to vote.");
+//  }
+//  var itemPrice = 50;
+//  var availableBalance = 30;
+//  if (availableBalance >= itemPrice) {
+//    console.log("You can proceed with the purchase.");
+//  } else {
+//    console.log("Insufficient funds to make the purchase.");
+//  }
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
